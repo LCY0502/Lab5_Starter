6f0edfb912f3d0470ed319f6ef4ead8ab19765e8
LCY0502 <cyl027@ucsd.edu>
2025-04-23 23:21:22 -0700

    add img changing when speak

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,12 +3,15 @@
 window.addEventListener('DOMContentLoaded', init);
 
 const synth = window.speechSynthesis;
-
+const smiling_path = "assets/images/smiling.png";
+const talking_path = "assets/images/smiling-open.png"
 // Input area
 let voice_select; 
 let text_area;
-
 let talk_btn;
+
+let img;
+// Function parameter
 let voices = [];
 let voice = {name: ""};
 
@@ -16,12 +19,12 @@ function init() {
   voice_select = document.getElementById("voice-select");
   text_area = document.getElementById("text-to-speak");
   talk_btn = document.querySelector("button");
-
+  img = document.querySelector("img");
+  
   talk_btn.onclick = utter_text;
   voice_select.onchange = voice_change;
 
   populateVoiceList();
-  voice_change();
 }
 
 function populateVoiceList() {
@@ -43,8 +46,13 @@ function populateVoiceList() {
     option.setAttribute("name", voices[i].name);
     voice_select.appendChild(option);
   }
+
+  // Reset
+  voice_select.selectedIndex = 0;
 }
 
+// parameter (voices, voice)
+// update value in "voice"
 function voice_change(){
   const option = voice_select[voice_select.selectedIndex];
   const selected_name = option.getAttribute("name");
@@ -56,9 +64,21 @@ function voice_change(){
   }
 }
 
+// parameter (voice, text_area.value)
 function utter_text(){
   if(voice.name == "" || text_area.value == "") return;
   const utter = new SpeechSynthesisUtterance(text_area.value);
   utter.voice = voice;
+
+  utter.onstart = ()=>{
+    img.src = talking_path;
+  }
+  utter.onend = ()=>{
+    img.src = smiling_path;
+  }
   synth.speak(utter);
+
+
 }
+
+
